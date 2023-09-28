ccff57c4e0767236b61b0f2521f40ce69101bd65
Ahmed Abdullahi <aabdullahi@costar.com>
2023-09-28 14:20:59 -0400

    Revert "added comments, done!"
    This reverts commit e7dbea8d9cd23fd9c0a6b8f6b7572f5a8b3f90ec.

--- my-repo-viewer/src/App.js
+++ my-repo-viewer/src/App.js
@@ -3,39 +3,31 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
-  //state to store GitHub usernames
   const [username, setUsername] = useState("");
-  //state to store list of repositories
+
   const [repos, setRepos] = useState([]);
 
-  //function to fetch repositories for a given username from the backend
   const fetchRepos = async () => {
     try {
-      //making a GET request to the backend giving the username
       const response = await axios.get(`/repos/${username}`);
-      //updating the repositotry state with the fetched repos
+
       setRepos(response.data);
     } catch (error) {
-      //could not fetch repos
       console.error("Error fetching repos:", error);
     }
   };
 
-
-
   return (
     <div className="App">
-      {/*input field for user to enter GitHub username*/}
       <input
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Enter GitHub username"
       />
-      {/*button to trigger fetching the repos*/}
+
       <button onClick={fetchRepos}>Fetch Repos</button>
 
       <ul>
-        {/*for each repo in the state, display its name in a list*/}
         {repos.map((repo) => (
           <li key={repo.id}>{repo.name}</li>
         ))}
